ace088ad30614ecc7703812ec87e28e6a4723d83
dataweek101 <42237891+dataweek101@users.noreply.github.com>
2018-08-09 12:08:49 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -7,4 +7,6 @@ set -e
 # pip3 install matplotlib==2.1.2
 # pip3 install scikit-learn==0.19.1
 
-
+DATADIR=/usr/local/share/datasets/
+mkdir -p $DATADIR
+wget -O $DATADIR/house_data.csv https://assets.datacamp.com/production/repositories/3325/datasets/3d68eaa3f92eb7b5d049f1f9b9ed94854323df1d/house_data.csv
