f2765087d8eb04140e483502590f633eb4da1e7f
dataweek101 <42237891+dataweek101@users.noreply.github.com>
2018-08-22 17:40:22 +0200

    updated req packages

--- requirements.sh
+++ requirements.sh
@@ -4,7 +4,7 @@ set -e
 ##### Install specific package versions with pip #####
 
 # pip3 install pandas==0.22.0
-# pip3 install matplotlib==2.1.2
+pip3 install matplotlib==2.1.2
 # pip3 install scikit-learn==0.19.1
 
 DATADIR=/usr/local/share/datasets/
